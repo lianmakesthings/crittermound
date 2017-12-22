a0c26f5f03b6f9d4ff26c047fa00755ae2206aaa
Unknown <draggili@gmail.com>
2017-12-22 01:52:25 +0100

    show stats in popover

--- src/components/Critter.vue
+++ src/components/Critter.vue
@@ -2,80 +2,69 @@
   <div>
     <b-progress v-if="showProgressBar" :value="health" :max="maxHealth" animated></b-progress>
     <div class="d-flex justify-content-around">
-      <div id="totalDetails" @mouseenter="showDetails" @mouseleave="hideDetails">
-        <span>{{ critter.score }}</span>
-        <div v-if="showTotalDetails">
-          <div>Generation: {{ critter.generation }}</div>
-          <div>Birth Order: {{ critter.id }}</div>
-          <div>Total Score: {{ critter.baseScore }}</div>
-          <div>Bonus Score: {{ critter.bonusScore }}</div>
-        </div>
+      <div>
+        <div :id="'totalDetails-'+critter.id">{{ critter.score }}</div>
+        <b-popover title="Stats" triggers="hover focus" :target="'totalDetails-'+critter.id">
+          <div>Generation: {{critter.generation}}</div>
+          <div>Birth Order: {{critter.id}}</div>
+          <div>Total Score: {{critter.baseScore}}</div>
+          <div>Bonus Score: {{critter.bonusScore}}</div>
+        </b-popover>
       </div>
-      <div id="vitalityDetails" @mouseenter="showDetails" @mouseleave="hideDetails">
-        <span>{{ critter.traits[0].value }}</span>
-        <div v-if="showVitalityDetails">
+      <div>
+        <div :id="'vitalityDetails-'+critter.id">{{ critter.traits[0].value }}</div>
+        <b-popover title="Stats" triggers="hover focus" :target="'vitalityDetails-'+critter.id">
           <div>Base: {{ critter.traits[0].value }}</div>
           <div>Bonus: {{ critter.traits[0].bonus }}</div>
           <div>Value: {{ critter.traits[0].getTrueValue() }}</div>
           <div>Health: {{ critter.maxHealth }}</div>
           <div>Sod Production: {{ critter.sodPerSecond }} per sec.</div>
-        </div>
+        </b-popover>
       </div>
-      <div id="strengthDetails" @mouseenter="showDetails" @mouseleave="hideDetails">
-        <span>{{ critter.traits[1].value }}</span>
-        <div v-if="showStrengthDetails">
+      <div>
+        <div :id="'strengthDetails-'+critter.id">{{ critter.traits[1].value }}</div>
+        <b-popover title="Stats" triggers="hover focus" :target="'strengthDetails-'+critter.id">
           <div>Base: {{ critter.traits[1].value }}</div>
           <div>Bonus: {{ critter.traits[1].bonus }}</div>
           <div>Value: {{ critter.traits[1].getTrueValue() }}</div>
           <div>Carrying Capacity: {{ critter.carryPerSecond}} per sec.</div>
-        </div>
+        </b-popover>
       </div>
-      <div id="agilityDetails" @mouseenter="showDetails" @mouseleave="hideDetails">
-        <span>{{ critter.traits[2].value }}</span>
-        <div v-if="showAgilityDetails">
+      <div>
+        <div :id="'agilityDetails-'+critter.id">{{ critter.traits[2].value }}</div>
+        <b-popover title="Stats" triggers="hover focus" :target="'agilityDetails-'+critter.id">
           <div>Base: {{ critter.traits[2].value }}</div>
           <div>Bonus: {{ critter.traits[2].bonus }}</div>
           <div>Value: {{ critter.traits[2].getTrueValue() }}</div>
           <div>Speed: {{ critter.actionTimeSeconds }} seconds</div>
-        </div>
+        </b-popover>
       </div>
-      <div id="biteDetails" @mouseenter="showDetails" @mouseleave="hideDetails">
-        <span>{{ critter.traits[3].value }}</span>
-        <div v-if="showBiteDetails">
+      <div>
+        <div :id="'biteDetails-'+critter.id">{{ critter.traits[3].value }}</div>
+        <b-popover title="Stats" triggers="hover focus" :target="'biteDetails-'+critter.id">
           <div>Base: {{ critter.traits[3].value }}</div>
           <div>Bonus: {{ critter.traits[3].bonus }}</div>
           <div>Value: {{ critter.traits[3].getTrueValue() }}</div>
           <div>Strength Bonus: {{ critter.strengthBonus }}</div>
           <div>Farm Production: {{ critter.grassPerSecond }} per sec.</div>
-        </div>
+        </b-popover>
       </div>
-      <div id="stingDetails" @mouseenter="showDetails" @mouseleave="hideDetails">
-          <span>{{ critter.traits[4].value }}</span>
-          <div v-if="showStingDetails">
-            <div>Base: {{ critter.traits[4].value }}</div>
-            <div>Bonus: {{ critter.traits[4].bonus }}</div>
-            <div>Value: {{ critter.traits[4].getTrueValue() }}</div>
-            <div>Agility Bonus {{ critter.agilityBonus }}</div>
-            <div>Mine Production: {{ critter.dirtPerSecond }} per sec.</div>
-          </div>
-        </div>
-      </td>
+      <div>
+        <div :id="'stingDetails-'+critter.id">{{ critter.traits[4].value }}</div>
+        <b-popover title="Stats" triggers="hover focus" :target="'stingDetails-'+critter.id">
+          <div>Base: {{ critter.traits[4].value }}</div>
+          <div>Bonus: {{ critter.traits[4].bonus }}</div>
+          <div>Value: {{ critter.traits[4].getTrueValue() }}</div>
+          <div>Agility Bonus {{ critter.agilityBonus }}</div>
+          <div>Mine Production: {{ critter.dirtPerSecond }} per sec.</div>
+        </b-popover>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
   export default {
-    data() {
-      return {
-        totalDetails: false,
-        strengthDetails: false,
-        vitalityDetails: false,
-        agilityDetails: false,
-        biteDetails: false,
-        stingDetails: false
-      }
-    },
     props: {
       critterId: {
         required: true,
@@ -99,9 +88,6 @@
       },
       maxHealth() {
         return this.critter.maxHealth
-      },
-      showTotalDetails() {
-        return this.totalDetails;
       },
       showVitalityDetails() {
         return this.vitalityDetails;
