c706cb34a0316d3d487927712999a23a76c12913
lian <draggili@gmail.com>
2018-04-19 21:52:54 +0200

    change factory tests

--- spec/lib/CritterFactorySpec.js
+++ spec/lib/CritterFactorySpec.js
@@ -8,11 +8,7 @@ describe('CritterFactory', () => {
 
     it('should return default Critter', () => {
         const critter = CritterFactory.default(someId, someGeneration, someGender);
-        expect(critter instanceof Critter).toBe(true);
-        expect(critter.id).toBe(someId);
-        expect(critter.generation).toBe(someGeneration);
-        expect(critter.gender).toBe(someGender);
-        expect(critter.rank).toBe(Critter.RANK_RECRUIT);
-        expect(critter.traits[0].base).toBe(5);
+        const defaultCritter = new Critter(someId, someGeneration, someGender);
+        expect(critter).toEqual(defaultCritter);
     })
 });
