f512c0d82f44da52cac3070ebbf2c7aa572aaa2a
lian <draggili@gmail.com>
2018-07-01 16:13:11 +0200

    refactor test

--- spec/lib/SodProductionSpec.js
+++ spec/lib/SodProductionSpec.js
@@ -4,40 +4,32 @@ import CritterFactory from '../../src/lib/CritterFactory';
 import Critter from '../../src/lib/Critter';
 
 let sodProduction;
+const type = 'mine';
 describe('Sod production', () => {
     beforeEach(() => {
-        sodProduction = new SodProduction({});
+        const state = {
+            worker: {
+                mine: {
+                    productionProp: 'dirtPerSecond',
+                    critters: []
+                }
+            }
+        };
+        sodProduction = new SodProduction(state);
     });
 
     describe('getting the worker with lowest production', () => {
         it('should return Critter', () => {
-            const type = 'mine';
             const lowCritter = CritterFactory.default(1, 1, Critter.GENDER_MALE);
             const highCritter = CritterFactory.default(2, 1, Critter.GENDER_FEMALE);
             highCritter.traits[Trait.ID_STING].base = 10;
-            sodProduction.state = {
-                worker: {
-                    mine: {
-                        productionProp: 'dirtPerSecond',
-                        critters: [lowCritter, highCritter]
-                    }
-                }
-            };
+            sodProduction.state.worker[type].critters = [lowCritter, highCritter];
             expect(sodProduction.lowestWorker(type)).toBe(lowCritter)
         });
 
         it("should return null if production has no workers", () => {
-            const type = 'mine';
-            sodProduction.state = {
-                worker: {
-                    mine: {
-                        productionProp: 'dirtPerSecond',
-                        critters: []
-                    }
-                }
-            };
             expect(sodProduction.lowestWorker(type)).toBeNull()
         });
-    })
-    
+    });
+
 });
