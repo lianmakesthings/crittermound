7c0076a7f1fc543d563a3edd5f722677ef12bd8b
Lian <draggili@gmail.com>
2018-09-12 15:00:28 +0200

    deactivate coverage reports

--- tests/unit/karma.conf.js
+++ tests/unit/karma.conf.js
@@ -13,7 +13,7 @@ module.exports = function karmaConfig (config) {
     // 2. add it to the `browsers` array below.
     browsers: ['PhantomJS'],
     frameworks: ['mocha', 'sinon-chai', 'phantomjs-shim'],
-    reporters: ['spec', 'coverage'],
+    reporters: ['spec'],
     files: ['./index.js'],
     preprocessors: {
       './index.js': ['webpack', 'sourcemap']
@@ -22,12 +22,12 @@ module.exports = function karmaConfig (config) {
     webpackMiddleware: {
       noInfo: true
     },
-    coverageReporter: {
+    /*coverageReporter: {
       dir: './coverage',
       reporters: [
         { type: 'lcov', subdir: '.' },
         { type: 'text-summary' }
       ]
-    },
+    },*/
   })
 }
