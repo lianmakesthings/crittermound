d530987c8a8ec1484fccc25eafb398cee8c68f9d
Lian <draggili@gmail.com>
2018-09-16 16:39:13 +0200

    rename variable

--- tests/lib/store.spec.js
+++ tests/lib/store.spec.js
@@ -65,16 +65,16 @@ describe('The vuex store', () => {
   });
 
   describe("getters", () => {
-    let expectedState;
+    let mockedState;
     beforeEach(() => {
-      expectedState = JSON.parse(JSON.stringify(state));
+      mockedState = JSON.parse(JSON.stringify(state));
     });
 
     it('should return the entire state', (done) => {
       getStore((store) => {
-        store.replaceState(expectedState);
+        store.replaceState(mockedState);
 
-        expect(store.getters.entireState).to.equal(expectedState);
+        expect(store.getters.entireState).to.equal(mockedState);
         done();
       }, true)
     });
@@ -82,11 +82,11 @@ describe('The vuex store', () => {
     it('should return all critters', (done) => {
       const queen = {};
       const worker = {};
-      expectedState.royalHatchery.mother.critters.push(queen);
-      expectedState.worker.mine.critters.push(worker);
+      mockedState.royalHatchery.mother.critters.push(queen);
+      mockedState.worker.mine.critters.push(worker);
 
       getStore((store) => {
-        store.replaceState(expectedState);
+        store.replaceState(mockedState);
         const allCritters = store.getters.allCritters;
 
         expect(allCritters.length).to.equal(2);
@@ -99,11 +99,11 @@ describe('The vuex store', () => {
     it('should return all workers', (done) => {
       const queen = {};
       const worker = {};
-      expectedState.royalHatchery.mother.critters.push(queen);
-      expectedState.worker.mine.critters.push(worker);
+      mockedState.royalHatchery.mother.critters.push(queen);
+      mockedState.worker.mine.critters.push(worker);
 
       getStore((store) => {
-        store.replaceState(expectedState);
+        store.replaceState(mockedState);
         const allCritters = store.getters.allWorkers;
 
         expect(allCritters.length).to.equal(1);
@@ -118,10 +118,10 @@ describe('The vuex store', () => {
       const type = 'mother';
       const queen = {};
       const worker = {};
-      expectedState[location][type].critters.push(queen);
-      expectedState.worker.mine.critters.push(worker);
+      mockedState[location][type].critters.push(queen);
+      mockedState.worker.mine.critters.push(worker);
       getStore((store) => {
-        store.replaceState(expectedState);
+        store.replaceState(mockedState);
         const critters = store.getters.critters(location, type);
 
         expect(critters).to.include(queen);
@@ -132,9 +132,9 @@ describe('The vuex store', () => {
 
     it('should return number of total critters', (done) => {
       const totalCritters = 42;
-      expectedState.totalCritters = totalCritters;
+      mockedState.totalCritters = totalCritters;
       getStore((store) => {
-        store.replaceState(expectedState);
+        store.replaceState(mockedState);
 
         expect(store.getters.totalCritters).to.equal(totalCritters);
         done();
@@ -143,10 +143,10 @@ describe('The vuex store', () => {
 
     it('should return sod production part of state', (done) => {
       const sodProduction = {some : "value"};
-      expectedState.worker = sodProduction;
+      mockedState.worker = sodProduction;
 
       getStore((store) => {
-        store.replaceState(expectedState);
+        store.replaceState(mockedState);
 
         expect(store.getters.sodProduction).to.equal(sodProduction);
         done();
@@ -155,10 +155,10 @@ describe('The vuex store', () => {
 
     it('should return the total amount of sod', (done) => {
       const totalSod = 1337;
-      expectedState.totalSod = totalSod;
+      mockedState.totalSod = totalSod;
 
       getStore((store) => {
-        store.replaceState(expectedState);
+        store.replaceState(mockedState);
 
         expect(store.getters.totalSod).to.equal(totalSod);
         done();
@@ -169,10 +169,10 @@ describe('The vuex store', () => {
       const location = 'royalHatchery';
       const type = 'mother';
       const expectedMound = {some: "mound"};
-      expectedState[location][type] = expectedMound;
+      mockedState[location][type] = expectedMound;
 
       getStore((store) => {
-        store.replaceState(expectedState);
+        store.replaceState(mockedState);
 
         expect(store.getters.mound(location, type)).to.equal(expectedMound);
         done();
@@ -184,11 +184,11 @@ describe('The vuex store', () => {
       const expectedCritter = {id: critterId};
       const otherCritter = {id: 2};
 
-      expectedState.worker.mine.critters.push(expectedCritter);
-      expectedState.royalHatchery.female.critters.push(otherCritter);
+      mockedState.worker.mine.critters.push(expectedCritter);
+      mockedState.royalHatchery.female.critters.push(otherCritter);
 
       getStore((store) => {
-        store.replaceState(expectedState);
+        store.replaceState(mockedState);
 
         expect(store.getters.findCritter(critterId)).to.equal(expectedCritter);
         done();
@@ -197,9 +197,9 @@ describe('The vuex store', () => {
 
     it('should return number of boosts', (done) => {
       const expectedBoosts = 5;
-      expectedState.royalHatchery.boosts = expectedBoosts;
+      mockedState.royalHatchery.boosts = expectedBoosts;
       getStore((store) => {
-        store.replaceState(expectedState);
+        store.replaceState(mockedState);
 
         expect(store.getters.boosts).to.equal(expectedBoosts);
         done();
@@ -208,9 +208,9 @@ describe('The vuex store', () => {
 
     it('should return max number of boosts', (done) => {
       const expectedMaxBoosts = 9;
-      expectedState.royalHatchery.maxBoosts = expectedMaxBoosts;
+      mockedState.royalHatchery.maxBoosts = expectedMaxBoosts;
       getStore((store) => {
-        store.replaceState(expectedState);
+        store.replaceState(mockedState);
 
         expect(store.getters.maxBoosts).to.equal(expectedMaxBoosts);
         done();
@@ -219,9 +219,9 @@ describe('The vuex store', () => {
 
     it('should return whether state was saved', (done) => {
       const stateSaved = true;
-      expectedState.stateSaved = stateSaved;
+      mockedState.stateSaved = stateSaved;
       getStore((store) => {
-        store.replaceState(expectedState);
+        store.replaceState(mockedState);
 
         expect(store.getters.showStateSaved).to.equal(stateSaved);
         done();
@@ -230,9 +230,9 @@ describe('The vuex store', () => {
 
     it('should return new gene chance', (done) => {
       const newGeneChance = 9;
-      expectedState.newGeneChance = newGeneChance;
+      mockedState.newGeneChance = newGeneChance;
       getStore((store) => {
-        store.replaceState(expectedState);
+        store.replaceState(mockedState);
 
         expect(store.getters.newGeneChance).to.equal(newGeneChance);
         done();
@@ -241,9 +241,9 @@ describe('The vuex store', () => {
 
     it('should return array of unlocked gene ids', (done) => {
       const unlockedGeneId = 2;
-      expectedState.unlockedGenes.push(unlockedGeneId);
+      mockedState.unlockedGenes.push(unlockedGeneId);
       getStore((store) => {
-        store.replaceState(expectedState);
+        store.replaceState(mockedState);
 
         expect(store.getters.unlockedGenes).to.include(unlockedGeneId);
         done();
@@ -252,14 +252,14 @@ describe('The vuex store', () => {
 
     it('should return allocation of the royal hatchery', (done) => {
       const sizeEach = 5;
-      expectedState.royalHatchery.female.size = sizeEach;
-      expectedState.royalHatchery.male.size = sizeEach;
-      expectedState.royalHatchery.female.critters.push({});
-      expectedState.royalHatchery.female.critters.push({});
-      expectedState.royalHatchery.male.critters.push({});
+      mockedState.royalHatchery.female.size = sizeEach;
+      mockedState.royalHatchery.male.size = sizeEach;
+      mockedState.royalHatchery.female.critters.push({});
+      mockedState.royalHatchery.female.critters.push({});
+      mockedState.royalHatchery.male.critters.push({});
       const expectedAlloc = {current: 3, max: sizeEach*2};
       getStore((store) => {
-        store.replaceState(expectedState);
+        store.replaceState(mockedState);
 
         expect(store.getters.royalHatcheryAlloc).to.deep.equal(expectedAlloc);
         done();
@@ -268,16 +268,16 @@ describe('The vuex store', () => {
 
     it('should return allocation of the worker mound', (done) => {
       const sizeEach = 5;
-      expectedState.worker.mine.size = sizeEach;
-      expectedState.worker.farm.size = sizeEach;
-      expectedState.worker.carry.size = sizeEach;
-      expectedState.worker.factory.size = sizeEach;
-      expectedState.worker.mine.critters.push({});
-      expectedState.worker.carry.critters.push({});
-      expectedState.worker.carry.critters.push({});
+      mockedState.worker.mine.size = sizeEach;
+      mockedState.worker.farm.size = sizeEach;
+      mockedState.worker.carry.size = sizeEach;
+      mockedState.worker.factory.size = sizeEach;
+      mockedState.worker.mine.critters.push({});
+      mockedState.worker.carry.critters.push({});
+      mockedState.worker.carry.critters.push({});
       const expectedAlloc = {current: 3, max: sizeEach*4};
       getStore((store) => {
-        store.replaceState(expectedState);
+        store.replaceState(mockedState);
 
         expect(store.getters.workerAlloc).to.deep.equal(expectedAlloc);
         done();
@@ -285,11 +285,11 @@ describe('The vuex store', () => {
     });
 
     it('should return the unlock level for all achievemnts', (done) => {
-      const unlockedAchievements = [].concat(expectedState.achievements);
+      const unlockedAchievements = [].concat(mockedState.achievements);
       unlockedAchievements[4] = 2;
-      expectedState.achievements = unlockedAchievements;
+      mockedState.achievements = unlockedAchievements;
       getStore((store) => {
-        store.replaceState(expectedState);
+        store.replaceState(mockedState);
 
         expect(store.getters.unlockedAchievements).to.equal(unlockedAchievements);
         done();
@@ -298,12 +298,12 @@ describe('The vuex store', () => {
 
     it('should return the allocation of the army mound', (done) => {
       const size = 5;
-      expectedState.soldiers.army.size = size;
-      expectedState.soldiers.army.critters.push({});
-      expectedState.soldiers.army.critters.push({});
+      mockedState.soldiers.army.size = size;
+      mockedState.soldiers.army.critters.push({});
+      mockedState.soldiers.army.critters.push({});
       const expectedAlloc = {current: 2, max: size};
       getStore((store) => {
-        store.replaceState(expectedState);
+        store.replaceState(mockedState);
 
         expect(store.getters.armyAlloc).to.deep.equal(expectedAlloc);
         done();
@@ -311,12 +311,12 @@ describe('The vuex store', () => {
     });
 
     it('should return the achievement quota', (done) => {
-      const unlockedAchievements = [].concat(expectedState.achievements);
+      const unlockedAchievements = [].concat(mockedState.achievements);
       unlockedAchievements[4] = 2;
-      expectedState.achievements = unlockedAchievements;
+      mockedState.achievements = unlockedAchievements;
       const expectedQuota = {current: 2, max: 212};
       getStore((store) => {
-        store.replaceState(expectedState);
+        store.replaceState(mockedState);
 
         expect(store.getters.achievementQuota).to.deep.equal(expectedQuota);
         done();
@@ -324,9 +324,9 @@ describe('The vuex store', () => {
     });
 
     it('should return the current war state', (done) => {
-      expectedState.soldiers.currentWar = War.generateMap(Nation.CRICKETS);
+      mockedState.soldiers.currentWar = War.generateMap(Nation.CRICKETS);
       getStore((store) => {
-        store.replaceState(expectedState);
+        store.replaceState(mockedState);
 
         expect(store.getters.atWar).to.be.true;
         done();
@@ -335,9 +335,9 @@ describe('The vuex store', () => {
 
     it('should return the current war map', (done) => {
       const map = War.generateMap(Nation.CRICKETS);
-      expectedState.soldiers.currentWar = map;
+      mockedState.soldiers.currentWar = map;
       getStore((store) => {
-        store.replaceState(expectedState);
+        store.replaceState(mockedState);
 
         expect(store.getters.currentMap).to.equal(map.tiles);
         done();
