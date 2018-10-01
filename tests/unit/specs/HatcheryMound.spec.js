2bcb418edf39f6c058c8046a37c8b584bceb183d
Lian <draggili@gmail.com>
2018-10-01 10:19:16 +0200

    more hatchery mound tests

--- tests/unit/specs/HatcheryMound.spec.js
+++ tests/unit/specs/HatcheryMound.spec.js
@@ -15,6 +15,7 @@ describe('The Hatchery Mound View', () => {
   const critters = [critter];
   const mound = {size: 2};
   const propsData = {location, type};
+  const sorts = ['score', 'base'];
   let store;
 
   beforeEach(() => {
@@ -25,7 +26,8 @@ describe('The Hatchery Mound View', () => {
         },
         mound: () => {
           return () => mound
-        }
+        },
+        sorts: () => sorts
       }
     });
   });
@@ -47,4 +49,71 @@ describe('The Hatchery Mound View', () => {
     expect(critterWrapper.attributes('showprogressbar')).not.to.be.defined;
   });
 
+  it('should show buttons to use critters', () => {
+    const hatcheryMoundWrapper = shallowMount(HatcheryMound, {propsData, store, localVue});
+    const replaceParentButton = hatcheryMoundWrapper.find(`#replace-parent-${location}-${type}`);
+    const addWorkerButton = hatcheryMoundWrapper.find(`#add-worker-${location}-${type}`);
+    const addSoldierButton = hatcheryMoundWrapper.find(`#add-soldier-${location}-${type}`);
+
+    expect(replaceParentButton.attributes('type')).to.equal('button');
+    expect(replaceParentButton.text()).to.equal('Queen');
+    expect(addWorkerButton.attributes('type')).to.equal('button');
+    expect(addWorkerButton.text()).to.equal('Worker');
+    expect(addSoldierButton.attributes('type')).to.equal('button');
+    expect(addSoldierButton.text()).to.equal('Army');
+  });
+
+  it('should replace parent', () => {
+    const stubs = {
+      'critter-header': true,
+      'critter': true,
+      'b-dropdown': true
+    };
+    sinon.stub(store, 'dispatch');
+    const hatcheryMoundWrapper = mount(HatcheryMound, {propsData, store, localVue, stubs});
+    const replaceParentButton = hatcheryMoundWrapper.find(`#replace-parent-${location}-${type}`);
+
+    replaceParentButton.trigger('click');
+    expect(store.dispatch).to.have.been.calledWith('replaceParent', {location, type});
+  });
+
+  it('should add worker', () => {
+    const stubs = {
+      'critter-header': true,
+      'critter': true,
+      'b-dropdown': true
+    };
+    sinon.stub(store, 'dispatch');
+    const hatcheryMoundWrapper = mount(HatcheryMound, {propsData, store, localVue, stubs});
+    const addWorkerButton = hatcheryMoundWrapper.find(`#add-worker-${location}-${type}`);
+
+    addWorkerButton.trigger('click');
+    expect(store.dispatch).to.have.been.calledWith('addWorker', {location, type});
+  });
+
+  it('should add soldier', () => {
+    const stubs = {
+      'critter-header': true,
+      'critter': true,
+      'b-dropdown': true
+    };
+    sinon.stub(store, 'dispatch');
+    const hatcheryMoundWrapper = mount(HatcheryMound, {propsData, store, localVue, stubs});
+    const addSoldierButton = hatcheryMoundWrapper.find(`#add-soldier-${location}-${type}`);
+
+    addSoldierButton.trigger('click');
+    expect(store.dispatch).to.have.been.calledWith('addSoldier', {location, type});
+  });
+
+  it('should show dropdown menu for sorting', () => {
+    const stubs = {
+      'critter-header': true,
+      'critter': true,
+    };
+
+    const hatcheryMoundWrapper = mount(HatcheryMound, {propsData, store, localVue, stubs});
+    const sortDropdown = hatcheryMoundWrapper.find(`#sort-dropdown-${location}-${type}`);
+    const expectedText = `${sorts[0]}${sorts[1]}`;
+    expect(sortDropdown.text()).to.equal(expectedText);
+  });
 });
